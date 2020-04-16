4e844791d0c89f9f8e2339cf2285205c078b2235
Harshilbhardwaj47 <edwardkenway200017@gmail.com>
2020-04-16 03:33:20 -0700

    hacker rank currency reverse string problem solved comments

--- reversestring.java
+++ reversestring.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 
 public class str1 {
 	public static void main(String[] args) {
+		//defining a blank string
 		String b = "";
 		Scanner input = new Scanner(System.in);
+		//inouting a string nnamed a
 		String a = input.nextLine();
 		int n = a.length();
 		for (int i = n - 1; i >= 0; i--) {
