611e58bf4923365794d2b1bf9fd1dd9fb3be3ca2
Harshilbhardwaj47 <edwardkenway200017@gmail.com>
2020-04-16 12:18:14 -0700

    hacker rank currency reverse standard input output problem solved comments

---
+++ standardinputoutput1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class main10 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+
+}
+
