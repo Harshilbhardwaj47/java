0580ec72ad8f4979bf81d6d7593dd7b43ebd1ade
Harshilbhardwaj47 <edwardkenway200017@gmail.com>
2020-04-17 02:24:51 -0700

    main function complete

--- substrincomparision.java
+++ substrincomparision.java
@@ -23,7 +23,12 @@ public class substringcomparision{
 	}
 
 	public static void main(String[] args) {
-	
+	Scanner scan = new Scanner(System.in);
+		String s = scan.next();
+		int k = scan.nextInt();
+		scan.close();
+
+		System.out.println(getSmallestAndLargest(s, k));
 
 	}
 }
